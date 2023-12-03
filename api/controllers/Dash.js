5a421b92353dd6d4b2f018a19cf6377120d66ebe
Fabiano Carril <Fabiano_carril@hotmail.com>
2023-12-03 19:52:26 -0400

    Teste completo
    Foi implementado as datas, a parte de porcentagem e a quantidade de pessoas que fizeram o descarte

--- api/controllers/Dash.js
+++ api/controllers/Dash.js
@@ -1,31 +1,58 @@
 exports.Board = async (req, res) => {
     const dados = req.body;
     // #swagger.tags = ['Dashboard']
-      // #swagger.description = 'buscar dados de coleta'
+    // #swagger.description = 'buscar dados de coleta'
 
-    const totalPlastico = await db.residuo.sum('peso_em_kg', {
-        where: {
-            tipo_reisduo:"Plastico"
+    const TotalPessoas = await db.descarte.count({
+    distinct: true,
+    col: 'nome',
+    where: {
+          createdAt: {
+            [Op.between]: [new Date(dados.inicio), new Date(dados.fim)]
+          }
         }
     });
-    const totalVidro = await db.residuo.sum('peso_em_kg', {
+      
+
+
+    const totalPlastico = await db.coleta_peso.sum('peso_em_kg', {
         where: {
-            tipo_reisduo:"Vidro"
+            tipo_reisduo:"Plastico",
+            createdAt: { 
+                [Op.between]: [new Date(dados.inicio), new Date(dados.fim)]
+            }
         }
     });
-    const totalMetal = await db.residuo.sum('peso_em_kg', {
+    const totalVidro = await db.coleta_peso.sum('peso_em_kg', {
         where: {
-            tipo_reisduo:"Metal"
+            tipo_reisduo:"Vidro",
+            createdAt: { 
+                [Op.between]: [new Date(dados.inicio), new Date(dados.fim)]
+            }
         }
     });
-    const totalPapel = await db.residuo.sum('peso_em_kg', {
+    const totalMetal = await db.coleta_peso.sum('peso_em_kg', {
         where: {
-            tipo_reisduo:"Papel"
+            tipo_reisduo:"Metal",
+            createdAt: { 
+                [Op.between]: [new Date(dados.inicio), new Date(dados.fim)]
+            }
         }
     });
-    const totalOrganico = await db.residuo.sum('peso_em_kg', {
+    const totalPapel = await db.coleta_peso.sum('peso_em_kg', {
         where: {
-            tipo_reisduo:"Organico"
+            tipo_reisduo:"Papel",
+            createdAt: { 
+                [Op.between]: [new Date(dados.inicio), new Date(dados.fim)]
+            }
+        }
+    });
+    const totalOrganico = await db.coleta_peso.sum('peso_em_kg', {
+        where: {
+            tipo_reisduo:"Organico",
+            createdAt: { 
+                [Op.between]: [new Date(dados.inicio), new Date(dados.fim)]
+            }
         }
     });
         
@@ -33,6 +60,7 @@ exports.Board = async (req, res) => {
     const total= totalPapel+totalMetal+totalVidro+totalOrganico+totalPlastico;
         
     data={
+    quantPessoas:TotalPessoas,
     total:total,
     total_papel_kg:totalPapel,
     total_metal_kg:totalMetal,
@@ -40,29 +68,42 @@ exports.Board = async (req, res) => {
     total_organico_kg:totalOrganico,
     total_plastico_kg:totalPlastico
     }
+    const porcentagemPapel = (totalPapel / total) * 100;
+    const porcentagemMetal = (totalMetal / total) * 100;
+    const porcentagemVidro = (totalVidro / total) * 100;
+    const porcentagemOrganico = (totalOrganico / total) * 100;
+    const porcentagemPlastico = (totalPlastico / total) * 100;
     
-    if(dados){
-        const porcentagemPapel = (totalPapel / total) * 100;
-        const porcentagemMetal = (totalMetal / total) * 100;
-        const porcentagemVidro = (totalVidro / total) * 100;
-        const porcentagemOrganico = (totalOrganico / total) * 100;
-        const porcentagemPlastico = (totalPlastico / total) * 100;
-        
-        data = {
-            total_papel_kg: totalPapel,
-            total_metal_kg: totalMetal,
-            total_vidro_kg: totalVidro,
-            total_organico_kg: totalOrganico,
-            total_plastico_kg: totalPlastico,
-            porcentagem_papel: porcentagemPapel,
-            porcentagem_metal: porcentagemMetal,
-            porcentagem_vidro: porcentagemVidro,
-            porcentagem_organico: porcentagemOrganico,
-            porcentagem_plastico: porcentagemPlastico
-        };
-    // transforma para porcentagem
+    data_porcentagem = {
+        total_papel_kg: totalPapel,
+        total_metal_kg: totalMetal,
+        total_vidro_kg: totalVidro,
+        total_organico_kg: totalOrganico,
+        total_plastico_kg: totalPlastico,
+        porcentagem_papel: porcentagemPapel,
+        porcentagem_metal: porcentagemMetal,
+        porcentagem_vidro: porcentagemVidro,
+        porcentagem_organico: porcentagemOrganico,
+        porcentagem_plastico: porcentagemPlastico
     }
-    
+    if(!total){
+        return res.status(401),print("ERRO_Não_houve_coleta ")
+    }else{
+        if(dados.data=true){
+
+     return res.status(200).json(
+            data
+        )
+    }else{
+        return res.status(200).json(
+            data_porcentagem
+            )
+    }
+}
+
+ 
+
+
          
     
 
